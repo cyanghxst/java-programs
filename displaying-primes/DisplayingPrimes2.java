0b2af42e1afc4600d90d35e250e9bd6462360f63
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-09-25 11:04:59 -0700

    fix(DisplayingPrimes2): correct the while loop condition

--- displaying-primes/DisplayingPrimes2.java
+++ displaying-primes/DisplayingPrimes2.java
@@ -1,13 +1,13 @@
 public class DisplayingPrimes2 {
     public static void main(String[] args) {
-        final int NUMBER_OF_PRIMES = 50;
+        final int NUMBER_OF_PRIMES = 10;
         int count = 0;
         int number = 1;
 
         while (count <= NUMBER_OF_PRIMES) {
             if (isPrime(number)) {
-                System.out.print(number + " ");
                 count++;
+                System.out.println(count + ": " + number);
             }
             number++;
         }

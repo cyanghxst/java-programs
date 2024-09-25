e50d5915dc609c63ce04c5a1241d71052714d830
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-09-24 23:17:15 -0700

    fix(AdditionQuiz): correct the functionality

--- addition-quiz/AdditionQuiz.java
+++ addition-quiz/AdditionQuiz.java
@@ -4,16 +4,17 @@ public class AdditionQuiz {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int num1 = (int) Math.random() * 101;
-        int num2 = (int) Math.random() * 101;
+        int num1 = (int) (Math.random() * 101);
+        int num2 = (int) (Math.random() * 101);
         int answer = num1 + num2;
+        int input;
 
         do {
             System.out.print(num1 + " + " + num2 + " = ");
-            int input = scan.nextInt();
+            input = scan.nextInt();
 
             if (input != answer) {
-                System.out.println("\nTry Again!");
+                System.out.println("\nTry Again!\n");
             }
         } while (input != answer);
 

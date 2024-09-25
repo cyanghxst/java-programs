e563e97a62e6c3a5342a05ef3f671ebb193f4bda
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-09-25 08:05:06 -0700

    fix(CalculateSum): correct the input initialization

--- calculate-sum/CalculateSum.java
+++ calculate-sum/CalculateSum.java
@@ -5,11 +5,12 @@ public class CalculateSum {
         Scanner scan = new Scanner(System.in);
 
         int sum = 0;
+        int input;
 
         System.out.println("Enter a number (0 to stop): ");
 
         do {
-            int input = scan.nextInt();
+            input = scan.nextInt();
 
             if (input == 0) {
                 break;

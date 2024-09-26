0005f748072f4aaf66627d68df8fff5159ad3ce0
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-09-26 12:34:37 -0700

    feat(displaying-primes): add DisplayinPrimes3.java

---
+++ displaying-primes/DisplayingPrimes3.java
@@ -0,0 +1,27 @@
+public class DisplayingPrimes3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        final int NUMBERS_OF_PRIMES = 50;
+
+        int number = 1;
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 10; j++) {
+                if (number < 2) {
+                    continue;
+                }
+
+                for (int divisor = 2; divisor <= Math.sqrt(number); divisor++) {
+                    if (number % divisor == 0) {
+                        continue;
+                    }
+
+                    System.out.print(number + " ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}

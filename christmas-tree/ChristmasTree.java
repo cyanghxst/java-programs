464217dff33277e18018ff81649538ebd803576f
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-09-26 19:03:56 -0700

    feat(christmas-tree): add printBlank method

--- christmas-tree/ChristmasTree.java
+++ christmas-tree/ChristmasTree.java
@@ -8,12 +8,19 @@ public class ChristmasTree {
         int number = scan.nextInt();
 
         System.out.println();
+        printBlank(number);
         printStar(number);
     }
 
     public static void printStar(int number) {
         for (int i = 1; i <= number; i++) {
             System.out.print("*");
+        }
+    }
+
+    public static void printBlank(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.print(" ");
         }
     }
 }

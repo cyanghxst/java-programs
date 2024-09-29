33094e99be0ba5772243ff3610ff08e77d06f901
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-09-28 22:03:10 -0700

    feat(FloydsTriangle): add funtionality

--- pyramids/FloydsTriangle.java
+++ pyramids/FloydsTriangle.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
 
-public public class FloydsTriangle {
+public class FloydsTriangle {
     public static void main(String[] args) {
-        
+       Scanner scan = new Scanner(System.in);
+
+       System.out.print("Enter a number: ");
+       int number = scan.nextInt();
+
+       int integer = 1;
+
+       for (int i = 1; i <= number; i++) {
+           for (int j = 1; j <= i; j++) {
+               System.out.print(integer + " ");
+               integer++;
+           }
+           System.out.println();
+       }
     }
 }

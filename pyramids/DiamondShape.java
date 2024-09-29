71efea561a756c33e7fc0ba03a675599cdd29f25
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-09-29 09:09:30 -0700

    feat(DiamondShape): add for loop to print the bottom half of the diamond

--- pyramids/DiamondShape.java
+++ pyramids/DiamondShape.java
@@ -22,5 +22,21 @@ public class DiamondShape {
 
             System.out.println();
         }
+
+        for (int i = number - 1; i >= 1; i--) {
+            for (int j = 1; j <= number - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k);
+            }
+
+            for (int l = i - 1; l >= 1; l--) {
+                System.out.print(l);
+            }
+
+            System.out.println();
+        }
     }
 }

f4ff52086ef0b50c1aedcabe31730f556b5110c0
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-09-29 08:59:54 -0700

    feat(pyramids): add DiamondShape.java

---
+++ pyramids/DiamondShape.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public public class DiamondShape {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scan.nextInt();
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k);
+            }
+
+            System.out.println();
+        }
+    }
+}

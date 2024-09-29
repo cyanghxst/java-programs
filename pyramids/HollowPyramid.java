21cc423568bb0082c0119eff8baf3538bf524e61
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-09-28 22:23:57 -0700

    fix(HollowPyramid): correct the for loop to display the correct output

--- pyramids/HollowPyramid.java
+++ pyramids/HollowPyramid.java
@@ -11,8 +11,22 @@ public class HollowPyramid {
             for (int j = 1; j <= number - i; j++) {
                 System.out.print(" ");
             }
+
             System.out.print("*");
-            System.out.println();
+
+            for (int k = 1; k <= 2 * i - 3; k++) {
+                if (i == number) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            if (i == 1) {
+                System.out.println();
+            } else {
+                System.out.print("*\n");
+            }
         }
     }
 }

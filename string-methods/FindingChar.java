af1aa1c87c6fa5c16b92862551af9e8619c32f25
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-01 21:06:13 -0700

    feat(string-methods): add FindingChar.java

---
+++ string-methods/FindingChar.java
@@ -0,0 +1,8 @@
+public class FindingChar {
+    public static void main(String[] args) {
+        String message = "Water-based lube";
+        int index = message.indexOf('e', 3);
+
+        System.out.println(index);
+    }
+}

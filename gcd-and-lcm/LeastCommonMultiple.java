854d85c6f54aadc7c36851a53ac9d7243133291d
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-06 10:52:07 -0700

    feat(gcd-and-lcm): add LeastCommonMultiple.java

---
+++ gcd-and-lcm/LeastCommonMultiple.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LeastCommonMultiple {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the 1st number: ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Enter the 2nd number: ");
+        int num2 = scan.nextInt();
+
+        System.out.printf("\nThe ");
+    }
+}

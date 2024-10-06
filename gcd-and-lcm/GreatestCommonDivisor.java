e51fccb560258a7298275c4c48c5fd74cd769869
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-06 11:02:40 -0700

    feat(gcd-and-lcm): add GreatestCommonDivisor.java

---
+++ gcd-and-lcm/GreatestCommonDivisor.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the 1st number: ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Enter the 2nd number: ");
+        int num2 = scan.nextInt();
+
+        System.out.printf("\nThe greatest common divisor of %d and %d is %d\n", num1, num2, gcd(num1, num2));
+    }
+
+    public static int gcd(int num1, int num2) {
+        int dividend = Math.max(num1, num2);
+        int divisor = Math.min(num1, num2);
+        int remainder;
+
+        while (true) {
+            remainder = dividend % divisor;
+
+            if (remainder == 0) {
+                break;
+            }
+
+            dividend = divisor;
+            divisor = remainder;
+        }
+
+        return divisor;
+    }
+}

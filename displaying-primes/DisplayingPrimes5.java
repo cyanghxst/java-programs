408e1328666557f4d739e694f2254f9230ea956c
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-08 11:15:33 -0700

    feat(displaying-primes): add DisplayinPrimes5.java

---
+++ displaying-primes/DisplayingPrimes5.java
@@ -0,0 +1,33 @@
+public class DisplayingPrimes5 {
+    public static void main(String[] args) {
+        final int TOTAL_PRIMES = 50;
+        int number = 1;
+        int count = 1;
+
+        while (count <= TOTAL_PRIMES) {
+            boolean isPrime = true;
+
+            if (number < 2) {
+                isPrime = false;
+            }
+
+            for (int divisor = 2; divisor <= Math.sqrt(number); divisor++) {
+                if (number % number == 0) {
+                    isPrime = false;
+                }
+            }
+
+            if (isPrime) {
+                count++;
+
+                if (count % 10 == 0) {
+                    System.out.println(number);
+                } else {
+                    System.out.print(number + " ");
+                }
+            }
+
+            number++;
+        }
+    }
+}

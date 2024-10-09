33374ec9f8d9ea06a2d6bf748e982356508c9ffa
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-08 20:58:40 -0700

    fix(Tabulation): correct the functionality

--- tabulation/Tabulation.java
+++ tabulation/Tabulation.java
@@ -1,7 +1,7 @@
 public class Tabulation {
     public static void main(String[] args) {
         for (int i = 1; i <= 50; i++) {
-            System.out.printf("2 ^ %d = %.0f\n", i, Math.pow(i, 2));
+            System.out.printf("2 ^ %d = %.0f\n", i, Math.pow(2, i));
         }
     }
 }

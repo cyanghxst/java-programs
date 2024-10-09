f71d5bb6d54ef1911e5c7eb1e54d0c21902dd9ba
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-08 21:00:11 -0700

    feat(tabulation): add Tabulation2.java

---
+++ tabulation/Tabulation2.java
@@ -0,0 +1,13 @@
+public class Tabulation2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            int power = 1;
+
+            for (int j = 1; j <= i; j++) {
+                power *= 2;
+            }
+
+            System.out.printf("2 ^ %d = %d\n", i, power);
+        }
+    }
+}

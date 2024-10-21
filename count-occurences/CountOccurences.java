8d21c034f15a44ad3b78ab4bc0363dc6bd72f524
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-21 11:37:13 -0700

    feat(count-occurences): add CountOccurences.java

---
+++ count-occurences/CountOccurences.java
@@ -0,0 +1,31 @@
+public class CountOccurences {
+    public static void main(String[] args) {
+        final int SIZE = 100;
+
+        char[] array = genRandomArray(SIZE);
+    }
+
+    public static char[] genRandomArray(int size) {
+        char[] array = new char[size];
+        int random;
+
+        for (int i = 0; i < array.length; i++) {
+            random = (int) ((Math.random() * 16) + 97);
+            array[i] = (char) random;
+        }
+
+        return array;
+    }
+
+    public static int countChar(char ch, char[] array) {
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == ch) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}

f73354cc00da4321d7312b00e4847cba152bdfc7
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-23 17:03:01 -0700

    feat(ArrayReversing): add swap method

--- array-reversing/ArrayReversing.java
+++ array-reversing/ArrayReversing.java
@@ -24,4 +24,10 @@ public class ArrayReversing {
 
         return newArray;
     }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
 }

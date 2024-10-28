505bfc7572a6484e7102134bacca1f4ec1124e5e
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-28 11:02:27 -0700

    feat(file-io): add FileIO1.java

---
+++ file-io/FileIO1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class FileIO1 {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(Paths.get("file.txt"));
+             PrintWriter outFile = new PrintWriter(new FileWriter("output.txt"))) {
+            while (scanner.hasNextLine()) { 
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}

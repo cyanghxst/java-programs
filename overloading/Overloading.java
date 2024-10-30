8cd3c85a7eb4be434d4d6e184a08d7beea2e1f0d
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-30 11:13:16 -0700

    feat(Overloading): add debugging statements

--- overloading/Overloading.java
+++ overloading/Overloading.java
@@ -2,27 +2,26 @@ public class Overloading {
     public static void main(String[] args) {
         System.out.println(calculate(5, 3));
         System.out.println(calculate(5.0, 3));
-        System.out.println(calculate(5.0, 3));
         System.out.println(calculate(5.0, 3.0));
     }
 
     public static int calculate(int a, int b) {
-        System.out.println("\nFirst method");
+        System.out.println("\nFirst method is called");
         return a + b;
     }
 
     public static double calculate(double a, double b) {
-        System.out.println("\nSecond method");
+        System.out.println("\nSecond method is called");
         return a * b;
     }
 
     public static double calculate(int a, double b) {
-        System.out.println("\nThird method");
+        System.out.println("\nThird method is called");
         return a - b;
     }
 
     public static double calculate(double a, int b) {
-        System.out.println("\nForth method");
+        System.out.println("\nForth method is called");
         return a / b;
     }
 }

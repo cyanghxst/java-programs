62de3265334c8c214c0182f55adab738d0b72c8e
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-30 09:34:15 -0700

    feat(file-io): add FileIO5.java

---
+++ file-io/FileIO5.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class FileIO5 {
+	public static void main(String[] args) throws IOException {
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.print("Please enter the input file name: ");
+		String inputFileName = keyboard.nextLine();
+
+		System.out.print("\nPlease enter the output file name: ");
+		String outputFileName = keyboard.nextLine();
+
+		Scanner inFile = new Scanner(new File(inputFileName));
+		PrintWriter outFile = new PrintWriter(new PrintWriter(outputFileName));
+
+		while (inFile.hasNextLine()) {
+			String line = inFile.nextLine();
+            int indexOfSpace = line.indexOf(" ");
+
+            String printable = line.substring(indexOfSpace);
+			System.out.println(printable);
+			outFile.println(printable);
+		}
+
+		inFile.close();
+		outFile.close();
+	}
+}

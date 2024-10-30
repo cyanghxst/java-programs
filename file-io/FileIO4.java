7983461b426a6d7f06e0463067d748eeb8ce0b69
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-30 09:20:49 -0700

    fix(FileIO4): correct the class name

--- file-io/FileIO4.java
+++ file-io/FileIO4.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class FileIO2 {
+public class FileIO4 {
 	public static void main(String[] args) throws IOException {
 		Scanner keyboard = new Scanner(System.in);
 

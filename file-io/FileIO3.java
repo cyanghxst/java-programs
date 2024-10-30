d942b146242b94759f7977c66d1b856508178d0d
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-29 22:12:30 -0700

    fix(FileIO3): declare the missing variable

--- file-io/FileIO3.java
+++ file-io/FileIO3.java
@@ -5,6 +5,7 @@ public class FileIO3 {
     public static void main(String[] args) throws IOExceptions {
         Scanner inFile = new Scanner(new File("file.txt"));
         PrintWriter outFile = new PrintWriter(new FileWriter("out.txt"));
+        String line;
 
         while (inFile.hasNextLine()) {
             line = inFile.nextLine();

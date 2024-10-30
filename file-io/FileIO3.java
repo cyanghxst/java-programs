7eac07f7fd12aab0b442808fdf588bedb49e04d1
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-10-29 22:11:23 -0700

    feat(file-io): add FileIO3.java

---
+++ file-io/FileIO3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class FileIO3 {
+    public static void main(String[] args) throws IOExceptions {
+        Scanner inFile = new Scanner(new File("file.txt"));
+        PrintWriter outFile = new PrintWriter(new FileWriter("out.txt"));
+
+        while (inFile.hasNextLine()) {
+            line = inFile.nextLine();
+            System.out.println(line);
+            outFile = outFile.nextLine();
+        }
+
+        inFile.close();
+        outFile.close();
+    }
+}

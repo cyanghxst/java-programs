79598ec721480fdef3be6c823683eae8e55d2f1a
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-04 10:49:52 -0800

    fix(FibRecursion): correct base case

--- recursion/fib-recursion/FibRecursion.java
+++ recursion/fib-recursion/FibRecursion.java
@@ -4,7 +4,7 @@ public class FibRecursion {
     }
 
     public static int fibonacci(int num) {
-        if (num <= 1) {
+        if (num == 1 || num == 2) {
             return 1;
         }
         return fibonacci(num - 1) + fibonacci(num - 2);

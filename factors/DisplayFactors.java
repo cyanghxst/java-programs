974ecddf603d4ae74018f9ff07f4be6d5403cf2b
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-11 14:31:08 -0800

    fix(DisplayFactors): correct the prompt formatting for clarity

--- factors/DisplayFactors.java
+++ factors/DisplayFactors.java
@@ -4,7 +4,7 @@ public class DisplayFactors {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a positive integer: ");
         int number = scan.nextInt();
 
         displayFactors(number);

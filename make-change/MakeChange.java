32630425075a06a96e43aa6856d79b1f117d9fed
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-11 21:39:09 -0800

    feat(make-change): add MakeChange.java

---
+++ make-change/MakeChange.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MakeChange {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a purchase less than one dollar: ");
+        int purchase = scan.nextInt();
+    }
+
+    public static void makeChange(int purchase) {
+        if (purchase >= 100) {
+            System.out.println("The purchase is invalid");
+            return;
+        }
+
+        int[] coins = {25, 10, 5, 1};
+        String[] names = {"quarter", "dime", "nickle", "penny"};
+
+        for (int i = 0; i < coins.length; i++) {
+            System.out.print(change / coins[i] + " ");
+            System.out.println(names[i]);
+            change %= coins[i];
+        }
+    }
+}

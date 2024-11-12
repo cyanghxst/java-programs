f47af9f37a7aa97e002801fcd9faa4b599b9afc1
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-12 13:21:29 -0800

    feat(pyramids): add RightAlignedPyramid.java

---
+++ pyramids/RightAlignedPyramid.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RightAlignedPyramid {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scan.nextInt();
+
+        printPyramid(number);
+    }
+
+    public static void printPyramid(int number) {
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}

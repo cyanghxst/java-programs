1e28a1228628e4cd4f0f16c73ad4e87642b50a30
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-12 13:10:18 -0800

    fix(DisplayReverse): handle the case for 0 input

--- displaying-numbers/DisplayReverse.java
+++ displaying-numbers/DisplayReverse.java
@@ -11,6 +11,11 @@ public class DisplayReverse {
     }
 
     public static void reverse(int number) {
+        if (number == 0) {
+            System.out.println(0);
+            return;
+        }
+
         while (number > 0) {
             System.out.println(number % 10);
             number /= 10;

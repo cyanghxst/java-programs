4312258eba848ca3b5a871d7a9f6197cbe5f8534
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-13 00:17:05 -0800

    feat(consecutive-numbers): add ConsecutiveNumbers.java

---
+++ consecutive-numbers/ConsecutiveNumbers.java
@@ -0,0 +1,21 @@
+public class ConsecutiveNumbers {
+    public static void main(String[] args) {
+        int[] values = generateArray(20);
+
+        System.out.printf("The array %s three consecutive numbers with the same value", (isConsecutiveThree(values) ? "has" : "doesn't have"));
+    }
+
+    public static int[] generateArray(int size) {
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = (int) Math.random() * 10 + 1;
+        }
+
+        return array;
+    }
+
+    public static boolean isConsecutiveThree (int[] values) {
+        return true;
+    }
+}

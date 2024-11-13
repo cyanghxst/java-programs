1197b59662e2e984d5ed71bb0ce6ef337c6d341a
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-12 21:29:37 -0800

    feat(LuhnCheck): add sumOfOddDigits method

--- luhn-check/LuhnCheck.java
+++ luhn-check/LuhnCheck.java
@@ -47,6 +47,16 @@ public class LuhnCheck {
         return sum3 % 10 == 0;
     }
 
+    public static int sumOfOddDigits(int[] array) {
+        int sum = 0;
+
+        for (int i = array.length - 1; i >= 0; i -= 2) {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+
     public static int sumOfEvenDigits(int[] array) {
         int sum = 0;
 

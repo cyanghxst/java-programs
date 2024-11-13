f11dde448490beed70b01c77ea19a51f37df1177
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-13 01:30:06 -0800

    refactor(SumMajorDiagonal): reorganize the program

--- sum-matrix/SumMajorDiagonal.java
+++ sum-matrix/SumMajorDiagonal.java
@@ -9,16 +9,6 @@ public class SumMajorDiagonal {
         System.out.println(sumMajorDiagonal(matrix));
     }
 
-    public static void printMatrix(double[][] m) {
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[i].length; j++) {
-                System.out.print(m[i][j] + " ");
-            }
-
-            System.out.println();
-        }
-    }
-
     public static double[][] generateMatrix(int size) {
         double[][] m = new double[size][size];
 
@@ -30,6 +20,16 @@ public class SumMajorDiagonal {
         }
 
         return m;
+    }
+
+    public static void printMatrix(double[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.print(m[i][j] + " ");
+            }
+
+            System.out.println();
+        }
     }
 
     public static double sumMajorDiagonal(double[][] m) {

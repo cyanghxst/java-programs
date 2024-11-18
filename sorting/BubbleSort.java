b2b7d1e695864fa4f6759720f2f6236ba765f42e
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-18 10:15:26 -0800

    feat(sorting): add sort method

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -13,4 +13,14 @@ public public class BubbleSort {
 
         return array;
     }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i; i < array.length - 1; j++) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
 }

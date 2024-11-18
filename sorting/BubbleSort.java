33152334bfae556641a780e66c65cac9468e1934
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-18 12:13:11 -0800

    fix(BubbleSort): correct sort method

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -19,13 +19,18 @@ public class BubbleSort {
     }
 
     public static void sort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = i; i < array.length - 1; j++) {
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+        int lastIndex = array.length - 1;
+
+        for (int i = 1; i <= array.length; i++) {
+            for (int j = 0; j < lastIndex; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
             }
-            System.out.printf("Pass %d:\n%s\n", i + 1, java.util.Arrays.toString(array));
+
+            System.out.printf("\nPass %d:\n%s\n", i, java.util.Arrays.toString(array));
         }
     }
 }

8ae207d33a49666d3cefa8ab7972649bc1b4dddd
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-19 01:48:42 -0800

    feat(BubbleSort): change array initialization

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -1,6 +1,9 @@
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] array = shuffle(generate(10));
+        int[] array = generate(10);
+
+        shuffle(array);
+
         System.out.println("Original Array:\n" + java.util.Arrays.toString(array));
 
         sort(array);

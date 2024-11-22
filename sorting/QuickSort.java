175e14d6c260bb3b69b3008cb040a085e68b903a
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-21 21:42:08 -0800

    feat(Quicksort): add swap method

--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -29,4 +29,9 @@ public class QuickSort {
 
         }
     }
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
 }

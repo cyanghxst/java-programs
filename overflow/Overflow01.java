88fdf1e6a42e2fb6ec6d8aa2669f0e28d039f764
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-12-04 15:14:10 -0800

    feat(overflow): add Overflow01.java

---
+++ overflow/Overflow01.java
@@ -0,0 +1,9 @@
+public class Overflow01 {
+    public static void main(String[] args) {
+        int x = -270;
+        byte y = (byte) x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
